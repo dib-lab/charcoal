3aa6a100e149b95fc8674819117f7674e4bffb81
C. Titus Brown <titus@idyll.org>
2020-03-10 07:02:48 -0700

    upgrade plotting to remove zeros

--- cluster-and-plot.py
+++ cluster-and-plot.py
@@ -12,24 +12,33 @@ from sourmash import fig
 
 def load_and_normalize(filename):
     mat = genfromtxt(filename, delimiter=',')
-    assert mat.shape[0] == 347
-
-    to_delete = []
+    assert mat.shape[0] == 347            # number of metagenomes
     n_hashes = mat.shape[1]
+
+    # go through and normalize all the sample-presence vectors for each hash;
+    # track those with all 0s for later removal.
+    to_delete = []
     for i in range(n_hashes):
         if sum(mat[:, i]):
             mat[:, i] /= math.sqrt(np.dot(mat[:, i], mat[:, i]))
         else:
             to_delete.append(i)
 
-    #for row_n in reversed(to_delete):
-    #    mat = np.delete(mat, row_n, 0)
+    # remove all columns with zeros
+    print('removing {} null presence vectors'.format(len(to_delete)))
+    for row_n in reversed(to_delete):
+        mat = np.delete(mat, row_n, 1)
 
+    assert mat.shape[1] == n_hashes - len(to_delete)
+    n_hashes = mat.shape[1]
+
+    # construct distance matrix of all sample-presence vectors x themselves.
     D = np.zeros((n_hashes, n_hashes))
     for i in range(n_hashes):
         for j in range(n_hashes):
             D[i][j] = np.dot(mat[:, i], mat[:, j])
-            
+
+    # done!
     return D
         
         
@@ -59,7 +68,11 @@ def main():
     args = p.parse_args()
 
     mat = load_and_normalize(args.matrix_csv)
+
+    n_hashes = mat.shape[0]
     labels = [""]*mat.shape[0]
+    print('plotting {} hashes.'.format(n_hashes))
+
     x = fig.plot_composite_matrix(mat, labels,
                                   show_labels=False, show_indices=False,
                                   force=True)
