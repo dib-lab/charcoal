aaddc659a13af38f3400510ad831f92ad77d1891
C. Titus Brown <titus@idyll.org>
2020-05-02 10:19:43 -0700

    fixme

--- charcoal/just_taxonomy.py
+++ charcoal/just_taxonomy.py
@@ -98,7 +98,7 @@ def main():
     print(f'{f_major*100:.1f}% of hashes identify as {pretty_print_lineage(assign)}', file=report_fp)
     print(f'({identified_counts} identified hashes, {count} in most common)', file=report_fp)
     if f_major < 0.8:
-        print(f'** WARNING ** majority lineage is less than 80% of assigned lineages. Beware!')
+        print(f'** WARNING ** majority lineage is less than 80% of assigned lineages. Beware!', file=report_fp)
 
     clean_fp = gzip.open(args.clean, 'wt')
     dirty_fp = gzip.open(args.dirty, 'wt')
