b3f854a8e81884ef109ee07ac2d0e7c58521eb15
C. Titus Brown <titus@idyll.org>
2020-05-02 10:02:58 -0700

    pretty print lineages

--- charcoal/just_taxonomy.py
+++ charcoal/just_taxonomy.py
@@ -18,6 +18,19 @@ from sourmash.lca import LCA_Database
 from . import utils
 
 
+def pretty_print_lineage(lin):
+    if lin[-1].rank == 'strain':
+        strain = lin[-1].name
+        return f'{strain}'
+    elif lin[-1].rank == 'species':
+        species = lin[-1].name
+        return f'{species}'
+    elif not lin:
+        return f'** no assignment **'
+    else:
+        return f'{lin[-1].rank} {lin[-1].name}'
+
+
 def main():
     p = argparse.ArgumentParser()
     p.add_argument('--genome', help='genome file', required=True)
@@ -66,21 +79,24 @@ def main():
 
     # count them and find major
     counts = Counter()
+    identified_counts = 0
     for hashval, lineages in hash_assign.items():
-        for lineage in lineages:
-            counts[lineage] += 1
+        if lineages:
+            identified_counts += 1
+            for lineage in lineages:
+                counts[lineage] += 1
 
     # make sure it's strain or species level
     assign, count = next(iter(counts.most_common()))
     f_major = count / len(hash_assign)
-    print(f'{f_major*100:.1f}% of hashes identify as {assign}')
+    print(f'{f_major*100:.1f}% of hashes identify as {pretty_print_lineage(assign)}')
     if assign[-1].rank not in ('species', 'strain'):
         print(f'rank of major assignment is f{assign[-1].rank}; quitting')
         sys.exit(-1)
 
     report_fp = open(args.report, 'wt')
-    print(f'{f_major*100:.1f}% of hashes identify as {assign}', file=report_fp)
-    print(f'({len(entire_mh)} hashes, {count} in most common)', file=report_fp)
+    print(f'{f_major*100:.1f}% of hashes identify as {pretty_print_lineage(assign)}', file=report_fp)
+    print(f'({identified_counts} identified hashes, {count} in most common)', file=report_fp)
 
     clean_fp = gzip.open(args.clean, 'wt')
     dirty_fp = gzip.open(args.dirty, 'wt')
@@ -116,7 +132,7 @@ def main():
                     print(f'dirty! {ctg_lin}')
                     print('', file=report_fp)
                     print(f'---- contig {record.name}', file=report_fp)
-                    print(f'contig {record.name} dirty, REASON 2\nlineage is {ctg_lin}',
+                    print(f'contig {record.name} dirty, REASON 2\nlineage is {pretty_print_lineage(ctg_lin)}',
                           file=report_fp)
 
                 # summary reporting
@@ -127,18 +143,14 @@ def main():
                         for lineage in lineages:
                             ctg_counts[lineage] += 1
 
-                    print(f'\n** hashval lineage counts - {len(ctg_assign)}', file=report_fp)
-                    for lin, count in ctg_counts.most_common():
-                        print(f'   {count} {lin}', file=report_fp)
 
                     print(f'\n** hashval lca counts', file=report_fp)
                     for lin, count in ctg_tax_assign.most_common():
-                        print(f'   {count} {lin}', file=report_fp)
+                        print(f'   {count} {pretty_print_lineage(lin)}', file=report_fp)
+                    print(f'\n** hashval lineage counts - {len(ctg_assign)}', file=report_fp)
+                    for lin, count in ctg_counts.most_common():
+                        print(f'   {count} {pretty_print_lineage(lin)}', file=report_fp)
                 
-        # if long contig / many hashes, ...
-        
-        # if short contig / few hashes, ...
-
         if clean:
             clean_fp.write(f'>{record.name}\n{record.sequence}\n')
         else:
