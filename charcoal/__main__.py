72cf888146c4a1ebe1ae864d1996bba25371a2fe
C. Titus Brown <titus@idyll.org>
2020-05-16 10:20:30 -0700

    provide project initiation and configuration subcommands (#46)
    * create check subcommand; fix --configfile stuff
    
    * add showconf and init

--- charcoal/__main__.py
+++ charcoal/__main__.py
@@ -1,8 +1,10 @@
 "Enable python -m charcoal"
 import sys
 import os
-import click
 import subprocess
+import glob
+
+import click
 
 def get_snakefile_path():
     thisdir = os.path.dirname(__file__)
@@ -14,6 +16,42 @@ def get_package_configfile(filename):
     thisdir = os.path.dirname(__file__)
     configfile = os.path.join(thisdir, 'conf', filename)
     return configfile
+
+
+def run_snakemake(configfile, no_use_conda=False, verbose=False,
+                  extra_args=[]):
+    # find the Snakefile relative to package path
+    snakefile = get_snakefile_path()
+
+    # basic command
+    cmd = ["snakemake", "-s", snakefile]
+
+    # add --use-conda
+    if not no_use_conda:
+        cmd += ["--use-conda"]
+
+    # snakemake sometimes seems to want a default -j; set it to 1 for now.
+    # can overridden later on command line.
+    cmd += ["-j", "1"]
+
+    # add rest of snakemake arguments
+    cmd += list(extra_args)
+
+    # add defaults and system config files, in that order
+    configfiles = [get_package_configfile("defaults.conf"),
+                   get_package_configfile("system.conf"),
+                   configfile]
+    cmd += ["--configfile"] + configfiles
+
+    if verbose:
+        print('final command:', cmd)
+
+    # runme
+    try:
+        subprocess.check_call(cmd)
+    except subprocess.CalledProcessError as e:
+        print(f'Error in snakemake invocation: {e}', file=sys.stderr)
+        return e.returncode
 
 
 @click.group()
@@ -29,40 +67,81 @@ def cli():
 @click.argument('snakemake_args', nargs=-1)
 def run(configfile, snakemake_args, no_use_conda, verbose):
     "execute charcoal workflow (using snakemake underneath)"
-    # find the Snakefile relative to package path
-    snakefile = get_snakefile_path()
+    run_snakemake(configfile, no_use_conda, verbose, snakemake_args)
 
-    # basic command
-    cmd = ["snakemake", "-s", snakefile]
+@click.command()
+@click.argument('configfile')
+def check(configfile):
+    "check configuration"
+    run_snakemake(configfile, extra_args=['check'])
 
-    # add --use-conda
-    if not no_use_conda:
-        cmd += ["--use-conda"]
+@click.command()
+@click.argument('configfile')
+def showconf(configfile):
+    "show full configuration across default, system and project config files"
+    run_snakemake(configfile, extra_args=['showconf'])
 
-    # add defaults and system config files, in that order
-    cmd += ["--configfile", get_package_configfile("defaults.conf")]
-    cmd += ["--configfile", get_package_configfile("system.conf")]
 
-    # add explicit configfile from command line
-    cmd += ["--configfile", configfile]
-    # snakemake sometimes seems to want a default -j; set it to 1 for now.
-    # can overridden later on command line.
-    cmd += ["-j", "1"]
+@click.command()
+@click.argument('configfile')
+@click.option('--genome-dir', nargs=1)
+@click.option('-f', '--force', is_flag=True)
+def init(configfile, genome_dir, force):
+    "create a new, empty config file."
+    stubname = os.path.basename(configfile)
+    if configfile.endswith('.conf'):
+        stubname = stubname[:-5]
+    else:
+        configfile += '.conf'
 
-    # add rest of snakemake arguments
-    cmd += list(snakemake_args)
+    if os.path.exists(configfile) and not force:
+        print(f"** ERROR: configfile '{configfile}' already exists.")
+        return -1
 
-    if verbose:
-        print('final command:', cmd)
+    genome_list = f'{stubname}.genome-list.txt'
+    if genome_dir:
+        if os.path.exists(genome_list) and not force:
+            print(f"** ERROR: genome list file '{genome_list}' already exists.")
+            return -1
+        genomes = glob.glob(f'{genome_dir}/*.fa')
+        genomes += glob.glob(f'{genome_dir}/*.fna')
+        genomes += glob.glob(f'{genome_dir}/*.fa.gz')
+        genomes += glob.glob(f'{genome_dir}/*.fna.gz')
+        print(f'found {len(genomes)} genomes in {genome_dir}/*.{{fa,fna}}{{,.gz}}')
+        genomes = [ os.path.basename(g) for g in genomes ]
+        with open(genome_list, 'wt') as fp:
+            fp.write("\n".join(genomes))
+        print(f"created '{genome_list}' with {len(genomes)} genomes in it.")
 
-    # runme
-    try:
-        subprocess.check_call(cmd)
-    except subprocess.CalledProcessError as e:
-        print(f'Error in snakemake invocation: {e}', file=sys.stderr)
-        return e.returncode
+    print(f"creating configfile '{configfile}' for project '{stubname}'")
+    with open(configfile, 'wt') as fp:
+        fp.write(\
+f"""\
+# location for all generated files
+output_dir: output.{stubname}/
+
+# list of genome filenames to decontaminate
+genome_list: {stubname}.genome-list.txt
+
+# directory in which genome filenames live
+genome_dir: {genome_dir}
+
+# (optional) list of lineages for input genomes. comment out or leave
+# blank if none.
+provided_lineages:
+
+# match_rank is the rank _above_ which cross-lineage matches are considered
+# contamination. e.g. if set to 'superkingdom', then Archaeal matches in
+# Bacterial genomes will be contamination, but nothing else.
+#
+# values can be superkingdom, phylum, class, order, family, or genus.
+match_rank: order
+""")
 
 cli.add_command(run)
+cli.add_command(check)
+cli.add_command(showconf)
+cli.add_command(init)
 
 def main():
     cli()
