d8e46b7a6df69ad08b279dd90aae8e8fbde3de83
C. Titus Brown <titus@idyll.org>
2020-05-17 06:42:59 -0700

    add test of lca method, reason 1

--- tests/test_decontam.py
+++ tests/test_decontam.py
@@ -13,15 +13,17 @@ from sourmash.lca.command_index import load_taxonomy_assignments
 
 
 def make_lineage(lineage):
-   lin = lineage.split(';')
-   if len(lin) == 1:
-       lin = lineage.split(',')
-   lin = [ LineagePair(rank, n) for (rank, n) in zip(taxlist(), lin) ]
+    "Turn a ; or ,-separated set of lineages into a tuple of LineagePair objs."
+    lin = lineage.split(';')
+    if len(lin) == 1:
+        lin = lineage.split(',')
+    lin = [ LineagePair(rank, n) for (rank, n) in zip(taxlist(), lin) ]
 
-   return lin
+    return lin
 
 
 class ReportingCapture(object):
+    "Capture reporting output via 'report' fn."
     def __init__(self):
         self.lines = []
 
@@ -36,6 +38,7 @@ class ReportingCapture(object):
 
 
 class FakeFastaWriter(object):
+    "Test fixture to record FASTA output from ContigDecontaminator class."
     def __init__(self):
         self.n = 0
         self.bp = 0
@@ -46,17 +49,23 @@ class FakeFastaWriter(object):
         self.bp += len(record.sequence)
         self.n += 1
 
-def make_lca_and_lineages(match_files, lineages_csv, scaled, ksize, start_column=3):
+
+def make_lca_and_lineages(match_files, lineages_csv, scaled, ksize,
+                          start_column=3):
+    "Build an LCA_Database and LineageDB."
     lca_db = LCA_Database(ksize=ksize, scaled=scaled)
     lin_db = LineageDB()
 
+    # load matches into a list
     siglist = []
     for filename in match_files:
         siglist += list(sourmash.load_signatures(filename))
 
+    # pull in taxonomic assignments
     tax_assign, _ = load_taxonomy_assignments(lineages_csv,
                                               start_column=start_column)
 
+    # build database of matches & lineages!
     for ss in siglist:
         print(ss.name(), ss.minhash.scaled)
         ident = just_taxonomy.get_ident(ss)
@@ -69,8 +78,8 @@ def make_lca_and_lineages(match_files, lineages_csv, scaled, ksize, start_column
 
 
 def load_first_chunk(filename, chunksize=50000):
-    # load a chunk from a contiguous genome. eventually, need to fix to
-    # pay attention to contigs...
+    # load a first small chunk from a contiguous genome. eventually,
+    # need to fix to pay attention to contigs...
     with gzip.open(filename, 'rb') as fp:
         seqname = fp.readline().strip()[1:]
         seqdata = fp.read(chunksize).decode('utf-8')
@@ -474,9 +483,9 @@ def test_cleaner_gather_method_1():
     assert cleaner.n_reason_3 == 1        # should not be incremented
 
 
-def test_cleaner_lca_method_1():
-    # specify two bacterial contigs, and ask for phylum level cleaning.
-    # => one clean one dirty
+def test_cleaner_lca_method2_1():
+    # specify two bacterial contigs, and ask for genus level cleaning.
+    # => one clean one dirty, for reason 2
     genome_lineage = "Bacteria;Verrucomicrobia;Verrucomicrobiae;Verrucomicrobiales;Akkermansiaceae;Akkermansia;Akkermansia muciniphila"
     genome_lineage = make_lineage(genome_lineage)
 
@@ -511,3 +520,45 @@ def test_cleaner_lca_method_1():
     is_clean = cleaner.check_lca(chunk2[0], mh, report_fp)
     assert is_clean
     assert cleaner.n_reason_2 == 1        # should not be incremented
+
+
+def test_cleaner_lca_method1_1():
+    # label hashes in one contig with two different bacterial lineages
+    # => one dirty, for reason 1
+    genome_lineage = "Bacteria;Verrucomicrobia;Verrucomicrobiae;Verrucomicrobiales;Akkermansiaceae;Akkermansia;Akkermansia muciniphila"
+    genome_lineage = make_lineage(genome_lineage)
+
+    match_rank = 'genus'
+    empty_mh = sourmash.MinHash(n=0, ksize=31, scaled=10000)
+
+    matches_file1 = 'tests/test-data/2.fa.gz.gather-matches.sig.gz'
+    lineages_csv = 'test-data/test-match-lineages.csv'
+    lca_db, lin_db = make_lca_and_lineages([matches_file1],
+                                           lineages_csv,
+                                           empty_mh.scaled, empty_mh.ksize)
+
+    # now, hack the lineages so that it looks like the same sequence has
+    # a completely different lineage...
+    matches_2 = sourmash.load_signatures('tests/test-data/2.fa.gz.gather-matches.sig.gz')
+    matches_2 = list(matches_2)
+    fake_lin = 'Bacteria,Proteobacteria,Gammaproteobacteria,Alteromonadales,Shewanellaceae,Shewanella,Shewanella baltica'
+    fake_lin = make_lineage(fake_lin)
+
+    lin_db.insert('NC_011663', fake_lin)
+    for ss in matches_2:
+        lca_db.insert(ss, 'NC_011663')
+
+    cleaner = just_taxonomy.ContigsDecontaminator(genome_lineage,
+                                                  match_rank,
+                                                  empty_mh,
+                                                  lca_db, lin_db)
+    report_fp = StringIO()
+
+    chunk1 = load_first_chunk('test-data/genomes/2.fa.gz')
+
+    mh = empty_mh.copy_and_clear()
+    mh.add_sequence(chunk1[0].sequence, force=True)
+    is_clean = cleaner.check_lca(chunk1[0], mh, report_fp,
+                                 force_report=True)
+    assert not is_clean
+    assert cleaner.n_reason_1 == 1
