c84b8e11053ee47d9eff32aa216af7449250766a
Taylor Reiter <taylorreiter@gmail.com>
2021-05-26 10:19:19 -0700

    [MRG] flatten mh to allow gather_at_rank (#178)
    * flatten mh to allow gather_at_rank
    
    * add test and test data

--- tests/test_contigs_search.py
+++ tests/test_contigs_search.py
@@ -50,3 +50,23 @@ def test_2_loomba(location):
         saved_results = json.load(fp)
 
     assert this_results == saved_results
+
+@utils.in_tempdir
+def test_3_loomba(location):
+    # regression test/check for same results on Loomba
+    args = utils.Args()
+    args.genome = utils.relative_file("demo/genomes/LoombaR_2017__SID1050_bax__bin.11.fa.gz")
+    args.genome_sig = utils.relative_file("tests/test-data/loomba/LoombaR_2017__SID1050_bax__bin.11.fa.gz.sig")
+    args.matches_sig = utils.relative_file("tests/test-data/loomba/LoombaR_2017__SID1050_bax__bin.11.fa.gz.matches.abund.zip")
+    args.lineages_csv = utils.relative_file("tests/test-data/test-match-lineages.csv")
+    args.json_out = os.path.join(location, 'tax.json')
+    args.match_rank = 'genus'
+
+    status = contigs_search_taxonomy.main(args)
+
+    assert status == 0
+    assert os.path.exists(args.json_out)
+
+    with open(args.json_out, 'rt') as fp:
+        results = json.load(fp)
+        assert results != {}
