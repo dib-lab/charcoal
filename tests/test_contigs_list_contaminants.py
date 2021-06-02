c7f608f7df0f86f9caff3a2cb3a4299b623f2eea
taylorreiter <taylorreiter@gmail.com>
2021-06-02 15:00:56 -0700

    add test, fails with TypeError with abund sig

---
+++ tests/test_contigs_list_contaminants.py
@@ -0,0 +1,26 @@
+import os.path
+from . import pytest_utils as utils
+import json
+
+from charcoal import contigs_list_contaminants
+
+@utils.in_tempdir
+def test_1_loomba(location):
+    # regression test/check for same results on Loomba
+    args = utils.Args()
+    args.genome = utils.relative_file("demo/genomes/LoombaR_2017__SID1050_bax__bin.11.fa.gz")
+    args.genome_sig = utils.relative_file("tests/test-data/loomba/LoombaR_2017__SID1050_bax__bin.11.fa.gz.sig")
+    args.matches_sig = utils.relative_file("tests/test-data/loomba/LoombaR_2017__SID1050_bax__bin.11.fa.gz.matches.abund.zip")
+    args.lineages_csv = utils.relative_file("tests/test-data/test-match-lineages.csv")
+    args.hitlist = utils.relative_file("tests/test-data/loomba-hit-list.csv")
+    args.json_out = os.path.join(location, 'tax.json')
+    args.match_rank = 'genus'
+
+    status = contigs_list_contaminants.main(args)
+
+    assert status == 0
+    assert os.path.exists(args.json_out)
+
+    with open(args.json_out, 'rt') as fp:
+        results = json.load(fp)
+        assert results != {}
