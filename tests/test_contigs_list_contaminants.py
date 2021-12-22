979e8ee41f24a3b350720b3d32824285bb2637ae
C. Titus Brown <titus@idyll.org>
2021-12-22 11:19:38 -0800

    fixed test_contigs_list_contaminants.py

--- tests/test_contigs_list_contaminants.py
+++ tests/test_contigs_list_contaminants.py
@@ -10,7 +10,8 @@ def test_1_loomba(location):
     args = utils.Args()
     args.genome = utils.relative_file("demo/genomes/LoombaR_2017__SID1050_bax__bin.11.fa.gz")
     args.genome_sig = utils.relative_file("tests/test-data/loomba/LoombaR_2017__SID1050_bax__bin.11.fa.gz.sig")
-    args.matches_sig = utils.relative_file("tests/test-data/loomba/LoombaR_2017__SID1050_bax__bin.11.fa.gz.matches.abund.zip")
+    args.matches_csv = utils.relative_file("tests/test-data/loomba/LoombaR_2017__SID1050_bax__bin.11.fa.gz.matches.csv")
+    args.databases = [utils.relative_file('tests/test-data/loomba/LoombaR_2017__SID1050_bax__bin.11.fa.gz.matches.zip')]
     args.lineages_csv = utils.relative_file("tests/test-data/test-match-lineages.csv")
     args.hitlist = utils.relative_file("tests/test-data/loomba-hit-list.csv")
     args.json_out = os.path.join(location, 'tax.json')
